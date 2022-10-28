c8fbd0fbfd226520a8243d075371018d28b0dce8
lm317379829 <lm317379829@163.com>
2022-10-28 17:34:24 +0800

    Update py_jrskbs.py

--- plugin/py_jrskbs.py
+++ plugin/py_jrskbs.py
@@ -37,27 +37,38 @@ class Spider(Spider):
 
 	def categoryContent(self,tid,pg,filter,extend):
 		result = {}
-		url = 'https://m.jrskbs.com'
+		url = 'http://www.freezb.live/'
 		rsp = self.fetch(url)
 		html = self.html(rsp.text)
-		aList = html.xpath("//div[contains(@class, 'contentList')]/a")
+		aList = html.xpath("//tr[@class='match_main']")
 		videos = []
-		numvL = len(aList)
-		pgc = math.ceil(numvL/15)
+		img = 'https://s1.ax1x.com/2022/10/07/x3NPUO.png'
 		for a in aList:
-			aid = a.xpath("./@href")[0]
-			aid = self.regStr(reg=r'/live/(.*?).html', src=aid)
-			img = a.xpath(".//div[@class='contentLeft']/p/img/@src")[0]
-			home = a.xpath(".//div[@class='contentLeft']/p[@class='false false']/text()")[0]
-			away = a.xpath(".//div[@class='contentRight']/p[@class='false false']/text()")[0]
-			rmList = a.xpath(".//div[@class='contentCenter']/p/text()")
-			remark = rmList[1].replace('|','').replace(' ','') + '|' + rmList[0]
-			videos.append({
-				"vod_id": aid,
-				"vod_name": home + 'vs' + away,
-				"vod_pic": img,
-				"vod_remarks": remark
-			})
+			urlList = a.xpath("./td[@class='update_data live_link']/a")
+			stat = a.xpath("./td[contains(@style, 'font-weight:bold')]/sapn/@title")[0]
+			time = a.xpath("./td[contains(@style, 'font-weight:bold')]/sapn/text()")[0]
+			if '比分' not in urlList[0].xpath("./text()")[0] and stat == '直播中':
+				remark = a.xpath(".//p[@class='raceclass matchcolor']/@title")[0].replace('直播','') + '|' + time
+				name = a.xpath("string(./td[4])").replace(' ','').replace('\tVS','VS')
+				if 'VS' not in name:
+					names = name.split('\t')
+					remark = names[0] + '|' + time
+					name = names[-1].replace('vs','VS')
+				aid = ''
+				for url in urlList:
+					title = url.xpath("./text()")[0]
+					aurl = url.xpath("./@href")[0]
+					#aurl = self.regStr(reg=r'/tv/(.*?).html', src=aurl)
+					if '比分' not in title:
+						aid = aid + title + '@@@' + aurl + '#'
+				videos.append({
+					"vod_id": name + '###' + remark.split('|')[0] + '###' + aid,
+					"vod_name": name,
+					"vod_pic": img,
+					"vod_remarks": remark
+				})
+		numvL = len(videos)
+		pgc = math.ceil(numvL/15)
 		result['list'] = videos
 		result['page'] = pg
 		result['pagecount'] = pgc
@@ -67,37 +78,30 @@ class Spider(Spider):
 
 	def detailContent(self,array):
 		aid = array[0]
-		url = "http://m.jrskbs.com/live/{0}.html".format(aid)
-		rsp = self.fetch(url)
-		root = self.html(rsp.text)
-		divContent = root.xpath("//div[@class='today']")[0]
-		home = divContent.xpath(".//p[@class='onePlayer homeTeam']/text()")[0]
-		away = divContent.xpath(".//div[3]/text()")[0].strip()
-		title = home + 'vs' + away
-		pic = divContent.xpath(".//img[@class='gameLogo1 homeTeam_img']/@src")[0]
-		typeName = divContent.xpath(".//div/p[@class='name1 matchTime_wap']/text()")[0]
-		remark = divContent.xpath(".//div/p[@class='time1 matchTitle']/text()")[0].replace(' ','')
+		aids = aid.split('###')
+		name = aids[0]
+		typeName = aids[1]
+		tus = aids[2].strip('#').split('#')
+		pic = 'https://s1.ax1x.com/2022/10/07/x3NPUO.png'
 		vod = {
-			"vod_id": aid,
-			"vod_name": title,
+			"vod_id": name,
+			"vod_name": name,
 			"vod_pic": pic,
 			"type_name": typeName,
 			"vod_year": "",
 			"vod_area": "",
-			"vod_remarks": remark,
+			"vod_remarks": '',
 			"vod_actor": '',
 			"vod_director":'',
 			"vod_content": ''
 		}
-		urlList = root.xpath("//div[@class='liveshow']/a")
-		playUrl = ''
-		for url in urlList:
-			name = url.xpath("./text()")[0]
-			purl = url.xpath("./@data-url")[0]
-			playUrl =playUrl + '{0}${1}#'.format(name, purl)
+		purl = ''
+		for tu in tus:
+			title = tu.split('@@@')[0]
+			uid = tu.split('@@@')[1]
+			purl = purl + '{0}${1}'.format(title,uid) + '#'
 		vod['vod_play_from'] = '体育直播'
-		vod['vod_play_url'] = playUrl
-
+		vod['vod_play_url'] = purl
 		result = {
 			'list': [
 				vod
@@ -111,41 +115,31 @@ class Spider(Spider):
 
 	def playerContent(self,flag,id,vipFlags):
 		result = {}
-		url = id
-		if '04stream' in url:
-			rsp = self.fetch(url)
-			html = rsp.text
-			strList = re.findall(r"eval\((.*?)\);", html)
-			fuctList = strList[1].split('+')
-			scrpit = ''
-			for fuc in fuctList:
-				if fuc.endswith(')'):
-					append = fuc.split(')')[-1]
-				else:
-					append = ''
-				Unicode = int(self.regStr(reg=r'l\((.*?)\)', src=fuc))
-				char = chr(Unicode % 256)
-				char = char + append
-				scrpit = scrpit + char
-			par = self.regStr(reg=r'/(.*)/', src=scrpit).replace(')', '')
-			pars = par.split('/')
-			infoList = strList[2].split('+')
-			str = ''
-			for info in infoList:
-				if info.startswith('O'):
-					Unicode = int(int(self.regStr(reg=r'O\((.*?)\)', src=info)) / int(pars[0]) / int(pars[1]))
-					char = chr(Unicode % 256)
-					str = str + char
-			purl = self.regStr(reg=r"play_url=\'(.*?)\'", src=str)
-			result["parse"] = 0
-		elif 'v.stnye.cc' in url:
-			purl = id
-			result["parse"] = 1
-		elif 'dplayer' in url:
-			url = 'https://m.jrskbs.com' + url
-			rsp = self.fetch(url)
-			purl = self.regStr(reg=r'var PlayUrl = \"(.*?)\"', src=rsp.text)
-			result["parse"] = 0
+		url = id.strip('#')
+		rsp = self.fetch(url)
+		root = self.html(rsp.text)
+		phpurl = root.xpath("//div[@class='media']/iframe/@src")[0]
+		headers = {
+			"Referer": url,
+			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36"
+		}
+		rsp = self.fetch(phpurl,headers=headers,cookies=rsp.cookies)
+		if 'm3u8' in rsp.text:
+			if 'm3u8.html?id=' in rsp.text:
+				purl = re.search(r'm3u8\.html\?id=(.*?m3u8.*?)\"', rsp.text).group(1)
+			else:
+				purl = re.search(r"url: \'(.*?)\'", rsp.text).group(1)
+			if not purl.startswith('http'):
+				purl = re.search(r"(.*)/", phpurl).group(1) + purl
+		else:
+			aurl = re.search(r"(.*)/", phpurl).group(1) + re.search(r'src=\"..(.*?)\"', rsp.text).group(1)
+			aheaders = {
+				"Referer": aurl,
+				"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36"
+			}
+			r = self.fetch(aurl, headers=aheaders, cookies=rsp.cookies)
+			purl = re.search(r"url: \'(.*?)\'", r.text).group(1)
+		result["parse"] = 0
 		result["playUrl"] = ''
 		result["url"] = purl
 		result["header"] = ''
