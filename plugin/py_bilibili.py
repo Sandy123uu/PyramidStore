ebd64dcaf429da89a5dc590f06f4ef05c9418b38
Sandy123uu <sandy123.uu@gmail.com>
2025-04-25 21:53:59 +0800

    Add files via upload

--- plugin/py_bilibili.py
+++ plugin/py_bilibili.py
@@ -2497,16 +2497,15 @@ class Spider(Spider):
     def _get_playerContent(self, result, aid, cid, epid):
         self.pC_urlDic[f'{aid}_{cid}'] = urlDic = {**self.pC_urlDic.get(f'{aid}_{cid}', {}), 'epid': epid}
         vodDefaultQn = self.userConfig['vodDefaultQn']
-        query={'avid':aid, 'cid': cid, 'qn':vodDefaultQn, 'fnval': 4048, 'fnver': 0, 'fourk': 1, 'from_client': 'BROWSER'}
         if epid:
             url = 'https://api.bilibili.com/pgc/player/web/v2/playurl?aid={}&cid={}&qn={}&fnval=4048&fnver=0&fourk=1&from_client=BROWSER'.format(aid, cid, vodDefaultQn)
         else:
+            arg={'avid':aid, 'cid': cid, 'qn':vodDefaultQn, 'fnval': 4048, 'fnver': 0, 'fourk': 1, 'from_client': 'BROWSER', 'gaia_source': 'pre-load', 'isGaiaAvoided': 'true'}
             if not self.session_vip.cookies:
-                query['try_look'] = 1
-            query['gaia_source'] = 'pre-load'
-            query['isGaiaAvoided'] = 'true'
-            url = 'https://api.bilibili.com/x/player/wbi/playurl'
-            jRoot = self._get_sth(url, 'fake', queryDict=query).json()
+                arg['try_look'] = 1
+            query = self.encrypt_wbi(**arg)[0]
+            url = f'https://api.bilibili.com/x/player/wbi/playurl?{query}'
+        jRoot = self._get_sth(url, 'vip').json()
         ssid = ''
         if jRoot['code'] == 0:
             if 'data' in jRoot:
